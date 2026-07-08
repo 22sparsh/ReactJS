cba683f684f267a171fba18a43016355e002fbe8
Sparsh Prachandia Jain <spjainprachandia@gmail.com>
2026-07-08 23:05:08 +0530

    Understanding COMPONENTS

---
+++ React-First-Learning/src/components/Card.jsx
@@ -0,0 +1,7 @@
+function Card(){
+    return <div className="card">
+      <h1>Sparsh</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore assumenda reiciendis earum ratione!</p>
+    </div>
+}
+export default Card;
