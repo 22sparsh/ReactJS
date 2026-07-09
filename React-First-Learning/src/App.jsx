f40c6ed5b714b552315a2e3db30e5110704cd250
Sparsh Prachandia Jain <spjainprachandia@gmail.com>
2026-07-10 00:36:45 +0530

    Understanding a bit more:

--- React-First-Learning/src/App.jsx
+++ React-First-Learning/src/App.jsx
@@ -3,10 +3,15 @@ import Card from './components/Card'
 
 function App() {
   return (
-    <>
-    <h1 id='null' >HELLO WORLD...</h1>
+    <div className='parent'>
+
+      
     <Card />
-    </>
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    </div>
   )
 }
 
--- React-First-Learning/src/components/Card.jsx
+++ React-First-Learning/src/components/Card.jsx
@@ -1,7 +1,14 @@
-function Card(){
-    return <div className="card">
-      <h1>Sparsh</h1>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore assumenda reiciendis earum ratione!</p>
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='card'>
+      <img src="https://images.unsplash.com/photo-1783431412513-8043c76cc987?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+      <h1>Sparsh Jain</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta sequi debitis velit dolorum quia veritatis. Consequuntur, est unde? Cumque ea laboriosam voluptas odit!</p>
+      <button>JTO</button>
     </div>
+  )
 }
-export default Card;
+
+export default Card
