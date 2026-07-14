540acb3511bc7a5bdfcad4b32b220cb071f9c9c5
Sparsh Prachandia Jain <spjainprachandia@gmail.com>
2026-07-15 01:04:47 +0530

    feat:Prop Driling Implemented in the Transactional Card

--- React-first-mini-project02/src/Components/BalanceCard.jsx
+++ React-first-mini-project02/src/Components/BalanceCard.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Tbutton from './Tbutton'
+import WCard from './WCard'
 
 const BalanceCard = () => {
   return (
-    <div className="b bg-white/45 rounded-4xl p-3 flex flex-col gap-2.5">
+    <div className="b bg-white/45 rounded-4xl p-3 flex flex-col gap-4.5">
         <h1 className='text-base'>TotalBalance</h1>
         <h1 className='font-bold text-4xl'>$31,180.<span className='text-lg text-gray-400'>24</span></h1>
         <div className="tbut flex gap-2">
@@ -11,8 +12,11 @@ const BalanceCard = () => {
     <Tbutton status="Send"/>
     <Tbutton status="Recieve"/>
         </div>
-<h1 className='text-xl font-semibold mt-2'>My cards</h1>
-<div className="cardsection"></div>
+<h1 className='text-xl font-semibold mt-3'>My cards</h1>
+<div className="w-full block h-full bg-amber-100 ">
+  <WCard />
+  <WCard />
+</div>
     </div>
   )
 }
--- React-first-mini-project02/src/Components/TransactionsCard.jsx
+++ React-first-mini-project02/src/Components/TransactionsCard.jsx
@@ -1,6 +1,45 @@
 import React from 'react'
 import Tslide from './Tslide'
 
+const transactions = [
+  {
+    cname: "Apple Inc",
+    camt: "-$45.00",
+    t: "30 min ago",
+    type: "brand",
+  },
+  {
+    cname: "Jerry Helfer",
+    camt: "-$235.00",
+    t: "1 day ago",
+    type: "person",
+  },
+  {
+    cname: "Dribbble",
+    camt: "-$145.00",
+    t: "2 day ago",
+    type: "brand",
+  },
+  {
+    cname: "Ekra Food",
+    camt: "-$25.00",
+    t: "13 feb 2026",
+    type: "person",
+  },
+  {
+    cname: "Paypal Payment",
+    camt: "+40295.00",
+    t: "12 feb 2026",
+    type: "brand",
+  },
+  {
+    cname: "Meta Inc",
+    camt: "-$44.00",
+    t: "11 Jan 2026",
+    type: "brand",
+  },
+];
+
 const TransactionsCard = () => {
   return (
     <div className="tbox rounded-4xl p-3 pt-5 bg-[#CCE5E3] flex flex-col gap-4 ">
@@ -9,21 +48,20 @@ const TransactionsCard = () => {
       <span className='text-xs font-semibold'>See ALL</span>
       </div>
       <div className="">
+        {transactions.map((item, index) => (
+  <React.Fragment key={index}>
+    <Tslide
+      cname={item.cname}
+      camt={item.camt}
+      t={item.t}
+      type={item.type}
+    />
 
-      <Tslide  cname='Apple Inc' camt="-$45.00" t="30 min ago"type="brand"/>
-      <span className="block w-[99%] ml-2 h-px mt-2 bg-black/15 rounded-2xl"></span>
-      <Tslide  cname='Jerry Helfer' type="person" camt="-$235.00" t="1 day ago"/>
-      <span className="block w-[99%] ml-2 h-px mt-2 bg-black/15 rounded-2xl"></span>
-
-      <Tslide  cname='Dribbble' camt="-$145.00" t="2 day ago"type="brand"/>
-      <span className="block w-[99%] ml-2 h-px mt-2 bg-black/15 rounded-2xl"></span>
-
-      <Tslide  cname='Ekra Food' type="person" camt="-$25.00" t="13 feb 2026"/>
-      <span className="block w-[99%] ml-2 h-px mt-2 bg-black/15 rounded-2xl"></span>
-
-      <Tslide  cname='Paypal Payment' type="brand" camt="+40295.00" t="12 feb 2026"/>
-      <span className="block w-[99%] ml-2 h-px mt-2 bg-black/15 rounded-2xl"></span>
-      <Tslide  cname='Meta Inc' type="brand" camt="-$44.00" t="11 Jan 2026"/>
+    {index !== transactions.length - 1 && (
+      <span className="block w99% ml-2 h-px mt-2 bg-black/15 rounded-2xl"></span>
+    )}
+  </React.Fragment>
+))}
       </div>
     </div>
   )
