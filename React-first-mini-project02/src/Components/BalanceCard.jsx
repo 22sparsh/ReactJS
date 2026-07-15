3b4254757b0139fbfb0440ab80942332afcd398d
Sparsh Prachandia Jain <spjainprachandia@gmail.com>
2026-07-15 14:30:01 +0530

    fix:Fixing the styling

--- React-first-mini-project02/src/Components/BalanceCard.jsx
+++ React-first-mini-project02/src/Components/BalanceCard.jsx
@@ -4,7 +4,7 @@ import WCard from './WCard'
 
 const BalanceCard = () => {
   return (
-    <div className="b bg-white/45 rounded-4xl p-3 flex flex-col gap-4.5">
+    <div className="b bg-white/65 rounded-4xl p-3 flex flex-col gap-4.5">
         <h1 className='text-base'>TotalBalance</h1>
         <h1 className='font-bold text-4xl'>$31,180.<span className='text-lg text-gray-400'>24</span></h1>
         <div className="tbut flex gap-2">
--- React-first-mini-project02/src/Components/Dashboard.jsx
+++ React-first-mini-project02/src/Components/Dashboard.jsx
@@ -9,7 +9,7 @@ import CreditScoreCard from "./CreditScoreCard";
 
 const Dashboard = () => {
   return (
-    <div className="min-h-screen p-5 mt-4">
+    <div className="min-h-full p-5 mt-2.5">
       <div className="mx-auto max-w-7xl space-y-6">
         {/* Top Section */}
         <div className="grid grid-cols-12 gap-6">
@@ -30,13 +30,13 @@ const Dashboard = () => {
                 Money Well?
               </h2>
 
-              <button className="mt-8 rounded-full bg-[#9db9ff] px-6 py-3 text-white font-medium shadow">
+              <button className="mt-14 rounded-full bg-[#9db9ff] px-6 py-3 text-black text-sm font-medium shadow">
                 Learn More
               </button>
 
               {/* Decorative circles */}
               <div className="absolute top-3 left-64 w-20 h-20 border-[1.5px] border-black/75 rounded-full"></div>
-              <div className="absolute right-4 top-12 w-20 h-20 border-[1.5px] border-blue-400 rounded-full"></div>
+              <div className="absolute right-4 top-12 w-20 h-20 border-[1.5px] border-blue-300 rounded-full"></div>
               <div className="absolute right-6 -top-3 w-20 h-20 border-[1.5px] border-black/75 rounded-full"></div>
             </div>
           </div>
