fb0433bd1d46c9c17bd9a85686de90b8b39f2843
Sparsh Prachandia Jain <spjainprachandia@gmail.com>
2026-07-24 17:59:56 +0530

    useState understanding with the Counter Example

--- React-First-Learning/src/App.jsx
+++ React-First-Learning/src/App.jsx
@@ -1,26 +1,26 @@
+import { useState } from 'react';
 import './App.css'
 import Card from './components/Cards/Card'
 
 function App() {
+  // let a=20;
+  // instead use the useState
+  const [counter, setcounter] = useState(0)
   function btnclick(){
     console.log("clicked" );
   }
   function btnclick2(elem){
     console.log(elem);
-    
   }
+  // function change(){
+  //   a=30;
+  // }
   return (
     <>
-
-      <div className='parent bg-green-950'>
-        <Card user="Sarah" btnTXT="NTO" />
-        <Card user="Sparsh" btnTXT="JTO" />
-        <Card user="Hora" btnTXT="FTO" />
-        <Card user="Sam" btnTXT="CTO" />
-      </div>
-      <button className='bg-amber-50/25 rounded-2xl hover:bg-red-200 cursor-pointer text-black p-2' onClick={function (elem){
-        btnclick2(elem.target.innerHTML);
-      }}>Click Me!</button>
+    <h1 className='text-black text-6xl font-extrabold'>{counter}</h1>
+    <button className='bg-amber-50/25 rounded-2xl hover:bg-red-200 cursor-pointer text-black p-2' onClick={()=>{
+      setcounter(counter+1)
+    }}>Click Me!</button>
     </>
   )
 }
