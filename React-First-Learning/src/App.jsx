3aeafed17957fd570a0622b77ce5c1c1b0210772
Sparsh Prachandia Jain <spjainprachandia@gmail.com>
2026-07-24 14:37:34 +0530

    Understanding about the functions.

--- React-First-Learning/src/App.jsx
+++ React-First-Learning/src/App.jsx
@@ -2,16 +2,19 @@ import './App.css'
 import Card from './components/Cards/Card'
 
 function App() {
+  function btnclick(elem){
+    console.log("clicked" + elem);
+  }
   return (
     <>
 
-      <div className='parent'>
+      <div className='parent bg-green-950'>
         <Card user="Sarah" btnTXT="NTO" />
         <Card user="Sparsh" btnTXT="JTO" />
         <Card user="Hora" btnTXT="FTO" />
         <Card user="Sam" btnTXT="CTO" />
       </div>
-      <h1 className='bg-amber-300'>Hello</h1>
+      <button className='bg-amber-50/25 rounded-2xl hover:bg-red-200 cursor-pointer text-black p-2' onClick={btnclick}>Click Me!</button>
     </>
   )
 }
