5f283db594cd011254c892ca83147887a7258780
Sparsh Prachandia Jain <spjainprachandia@gmail.com>
2026-07-25 19:57:41 +0530

    Two Way Binding Performed.

--- React-First-Learning/src/App.jsx
+++ React-First-Learning/src/App.jsx
@@ -1,6 +1,8 @@
+import { useState } from 'react';
 import './App.css'
 
 function App() {
+  const [name, setname] = useState('')
   const handleSubmit=(e)=>{
     e.preventDefault()
     console.log("hello");
@@ -10,7 +12,11 @@ function App() {
     <>
     <div className="h-screen w-screen flex justify-center items-center">
     <form className='flex justify-center items-center flex-col gap-5' onSubmit={(e)=>{handleSubmit(e)}}>
-    <input type="text" className='bg-blue-300 rounded-2xl p-2 text-black text-4xl'/>  
+    <input placeholder='Enter Your Name' type="text" className='bg-blue-300 rounded-2xl p-2 text-black text-4xl'
+    value={name} onChange={(e)=>{
+      setname(e.target.value)
+    }} />  
+
     <button className='text-4xl bg-red-900 p-4 rounded-2xl'>submit</button>
     </form>      
     </div>
