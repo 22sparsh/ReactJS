bfca98cf894ac86123acd71e1567a4d075e48101
Sparsh Prachandia Jain <spjainprachandia@gmail.com>
2026-07-25 19:44:43 +0530

    Understanding the preventDefault of the Forms in the React.

--- React-First-Learning/src/App.jsx
+++ React-First-Learning/src/App.jsx
@@ -1,149 +1,19 @@
-import { useState } from 'react';
 import './App.css'
 
 function App() {
-  const [Minute, setMinute] = useState(0)
-  const [disabled, setDisabled] = useState(false);
-  const [Hour, setHour] = useState(0)
-  const [Secound, setSecound] = useState(0)
-  const upwork = (whichtime) => {
-    if (whichtime === setHour) {
-      whichtime(prev => prev + 1)
-    }
-    else {
-      whichtime(prev => (prev + 1) % 60)
-    }
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+    console.log("hello");
   }
-  const downwork = (whichtime) => {
-    if (whichtime === setHour) {
-      whichtime(prev => Math.max(0, prev - 1))
-    }
-    else {
-      whichtime(prev => Math.max(0, (prev - 1) % 60))
-    }
-  }
-  function timer() {
-    const id = setInterval(() => {
-      setSecound(prevSec => {
-        if (prevSec <= 1) {
-          setMinute(prevMin => {
-            if (prevMin <= 0) {
-              setHour(prevHour => {
-                if (prevHour <= 0) {
-                  clearInterval(id)
-                  setDisabled(false);
-                  return 0;
-                }
-                else {
-                  setMinute(59)
-                  setSecound(59)
-                }
-                return prevHour - 1;
-              })
-              return 0
-            }
-            else {
-              setSecound(59)
-            }
-            return prevMin - 1
-          })
-        }
-        return prevSec - 1;
-      });
-    }, 1000);
-  }
-
-  const startTimer = () => {
-
-    if ((Minute > 0 || Hour > 0 || Secound > 0)) {
-      timer()
-      setDisabled(true);
-    }
-  }
+  
   return (
     <>
-      <div className="flex items-center justify-center h-screen bg-[#07051c]">
-        <div className=" flex p-10  flex-col gap-14">
-
-          <div className="flex text-9xl font-bold  items-center gap-12">
-            <div className="flex flex-col justify-center items-center gap-6">
-              <button
-                className="w-6 h-6 bg-white cursor-pointer"
-                style={{
-                  clipPath: "polygon(50% 0%, 0% 100%, 100% 100%)",
-                }}
-                onClick={function () {
-                  upwork(setHour)
-                }}
-              >
-              </button>
-              <span>{Hour > 9 ? `${Hour}` : `0${Hour}`}</span>
-              <button
-                className="w-6 h-6 bg-white cursor-pointer"
-                style={{
-                  clipPath: "polygon(0% 0%, 100% 0%, 50% 100%)",
-                }}
-                onClick={function () {
-                  downwork(setHour)
-                }}
-              >
-              </button>
-            </div>
-            <span>:</span>
-            <div className="flex flex-col justify-center items-center gap-6">
-              <button
-                className="w-6 h-6 bg-white cursor-pointer"
-                style={{
-                  clipPath: "polygon(50% 0%, 0% 100%, 100% 100%)",
-                }}
-                onClick={function () {
-                  upwork(setMinute)
-                }}
-              >
-              </button>
-              <span><span>{Minute > 9 ? `${Minute}` : `0${Minute}`}</span></span>
-              <button
-                className="w-6 h-6 bg-white cursor-pointer"
-                style={{
-                  clipPath: "polygon(0% 0%, 100% 0%, 50% 100%)",
-                }}
-                onClick={function () {
-                  downwork(setMinute)
-                }}
-              >
-              </button>
-            </div>
-            <span>:</span>
-            <div className="flex flex-col justify-center items-center gap-6">
-              <button
-                className="w-6 h-6 bg-white cursor-pointer"
-                style={{
-                  clipPath: "polygon(50% 0%, 0% 100%, 100% 100%)",
-                }}
-                onClick={function () {
-                  upwork(setSecound)
-                }}
-              >
-              </button>
-              <span><span>{Secound > 9 ? `${Secound}` : `0${Secound}`}</span></span>
-              <button
-                className="w-6 h-6 bg-white cursor-pointer"
-                style={{
-                  clipPath: "polygon(0% 0%, 100% 0%, 50% 100%)",
-                }}
-                onClick={function () {
-                  downwork(setSecound)
-                }}
-              >
-              </button>
-            </div>
-          </div>
-          <div className="flex justify-center items-center">
-            <button className='bg-red-300 text-black pt-3 pb-3 px-6 py-6 rounded-2xl text-6xl cursor-pointer' onClick={startTimer} disabled={disabled}>Start</button>
-          </div>
-
-        </div>
-      </div>
+    <div className="h-screen w-screen flex justify-center items-center">
+    <form className='flex justify-center items-center flex-col gap-5' onSubmit={(e)=>{handleSubmit(e)}}>
+    <input type="text" className='bg-blue-300 rounded-2xl p-2 text-black text-4xl'/>  
+    <button className='text-4xl bg-red-900 p-4 rounded-2xl'>submit</button>
+    </form>      
+    </div>
     </>
   )
 }
