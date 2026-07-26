d34182c673fdfbcc403c4d4ec5d9cd93fa8ccee9
Sparsh Prachandia Jain <spjainprachandia@gmail.com>
2026-07-26 22:04:11 +0530

    docs: understanding how does the useRef works using scrollto section example.

--- React-First-Learning/src/App.jsx
+++ React-First-Learning/src/App.jsx
@@ -1,19 +1,34 @@
 import { useState } from 'react';
 import './App.css'
 import { useEffect } from 'react';
+import { useRef } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0);
-  useEffect(() => {
-    document.title = count;
-  }, [count]);
+    const sectionRef = useRef();
+
+  const scrollToSection = () => {
+    sectionRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+
 
   return (
-    <>
-    <button onClick={() => setCount(count + 1)} className='bg-red-200 text-2xl text-black  p-4 active:bg-indigo-700 cursor-pointer'>
-      {count}
-    </button>
-    </>
+   <div>
+      <button
+        onClick={scrollToSection}
+        className="w-19 h-10 items-center flex justify-center bg-amber-50 rounded-2xl p-3 text-black active:bg-red-500"
+      >
+        Go Down
+      </button>
+<div className="h-screen block"></div>
+      <div className="h-200"></div>
+
+      <div
+        ref={sectionRef}
+        className="h-40 bg-green-500 flex items-center justify-center text-white"
+      >
+        Target Section
+      </div>
+    </div>
   )
 }
 
