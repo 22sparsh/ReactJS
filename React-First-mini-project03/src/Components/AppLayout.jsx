091ddaa1b678522be3abb5a2cd20142af328c5f2
Sparsh Prachandia Jain <spjainprachandia@gmail.com>
2026-07-27 00:43:41 +0530

    chore/feat:rename left and mid section with intent/connection between viewstab and activeviewstab.

--- React-First-mini-project03/src/Components/AppLayout.jsx
+++ React-First-mini-project03/src/Components/AppLayout.jsx
@@ -1,13 +1,37 @@
-import React from 'react'
-import LeftSection from './left/LeftSection'
-import MidSection from './Mid/MidSection'
+import React, { useState } from 'react'
+import LeftSection from './left/ViewsTab'
+import MidSection from './Mid/ActiveViewTab'
 import Note from './Right/Note'
 
 const AppLayout = () => {
+    let views = [
+    {
+      name: "Notes",
+      ico: "fa-regular fa-file-lines w-5  text-center text-base ml-2",
+      content:{}
+    },
+    {
+      name: "Starred",
+      ico: "fa-regular fa-star w-5  text-center text-base ml-2",
+      content:{}
+    },
+    {
+      name: "Archived",
+      ico: "fa-regular fa-bookmark w-5  text-center text-base ml-2 ",
+      content:{}
+    },
+    {
+      name: "Trash",
+      ico: "fa-regular fa-trash-can w-5  text-center text-base ml-2",
+      content:{}
+    }
+  ]
+  const [midsectiontopings, setmidsectiontopings] = useState(views[0])
+  
     return (
-        <div className="grid  grid-rows-[1fr_auto] grid-cols-[250px_260px_1fr] bg-white h-screen">
-            <LeftSection />
-            <MidSection />
+        <div className="grid  text-zinc-800 grid-rows-[1fr_auto] grid-cols-[250px_260px_1fr] bg-white h-screen">
+            <LeftSection views={views} status={setmidsectiontopings} />
+            <MidSection status={midsectiontopings} />
             <Note />
             <div className="col-span-3 border-t-2  border-gray-200">
                 Footer
--- React-First-mini-project03/src/Components/left/ViewsTab.jsx
+++ React-First-mini-project03/src/Components/left/ViewsTab.jsx
@@ -3,40 +3,24 @@ import Search from '../Utilities/Search';
 
 
 const LeftSection = (prop) => {
-
-  let views = [
-    {
-      name: "Notes",
-      ico: "fa-regular fa-file-lines w-5  text-center text-base ml-2"
-    },
-    {
-      name: "Starred",
-      ico: "fa-regular fa-star w-5  text-center text-base ml-2"
-    },
-    {
-      name: "Archived",
-      ico: "fa-regular fa-bookmark w-5  text-center text-base ml-2 "
-    },
-    {
-      name: "Trash",
-      ico: "fa-regular fa-trash-can w-5  text-center text-base ml-2"
-    }
-  ]
-
+  function handleViewClick(index) {
+    prop.status(prop.views[index])
+  }
   return (
-    <div className='flex flex-col gap-5 bg-gray-200 h-full'>
+    <div className='flex flex-col text-zinc-800 gap-5 bg-gray-200 h-full'>
       <div className=" flex h-15  justify-start items-center gap-3 p-3 border-b-2 border-gray-900 ">
-        <i className="fa-solid fa-wand-magic-sparkles text-xl"></i>
+        <img src="/logo.svg" alt="File" width={30} height={10} />
         <h1 className='text-xl font-bold'>NoteBench</h1>
       </div>
       <div className="text-base  font-medium flex flex-col gap-4">
         <h1 className='ml-3 text-base font-black'>Views</h1>
         <div className="text-base font-light flex flex-col justify-start items-center gap-3">
           {
-            views.map((ele, index) => (
+            prop.views.map((ele, index) => (              
               <div
                 key={index}
-                className="flex gap-1 items-center w-full hover:bg-white cursor-pointer"
+                className="flex gap-1 items-center w-full hover:bg-white cursor-pointer active:bg-indigo-400"
+                onClick={()=>{handleViewClick(index)}}
               >
                 <i className={ele.ico}></i>
                 <h1 className="font-light">{ele.name}</h1>
