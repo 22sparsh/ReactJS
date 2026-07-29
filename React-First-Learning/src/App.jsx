9036f7608916c38e1e2dfefde16749de134e2426
Sparsh Prachandia Jain <spjainprachandia@gmail.com>
2026-07-29 22:42:14 +0530

    state changes and Rendering concept

--- React-First-Learning/src/App.jsx
+++ React-First-Learning/src/App.jsx
@@ -1,24 +1,29 @@
-import './App.css'
-import axios from 'axios'
+import { useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  async function getData() {  
-    const {data}= await axios.get("https://jsonplaceholder.typicode.com/todos/1")
-      console.log(data);
-      
-    
+  const [c, setc] = useState(0);
+  // "Pressed" is logged only once because clicking the button sets
+  // c to 0 + 0 = 0. Since the state doesn't change, React doesn't
+  // re-render, so "Pressed" is logged only once.
+  if (c < 3) {
+    console.log("Pressed");
   }
   return (
     <>
-    <div className="">
-      <button 
-      className='bg-amber-50/50 p-3 rounded-2xl active:scale-50'
-      onClick={getData}
-      >Get Data
-      </button>
-    </div>
+      <div className="">
+        <button
+          className="bg-amber-50/50 p-3 rounded-2xl active:scale-50"
+          onClick={() => {
+            setc(c + c);
+          }}
+        >
+          {c}
+        </button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
