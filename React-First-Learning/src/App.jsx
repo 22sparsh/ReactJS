d6c6e8ac0bcd3faebdbe40ae66061e72bcda3f11
Sparsh Prachandia Jain <spjainprachandia@gmail.com>
2026-07-29 22:05:57 +0530

    Understanding the api calling

--- React-First-Learning/src/App.jsx
+++ React-First-Learning/src/App.jsx
@@ -1,8 +1,21 @@
 import './App.css'
 
 function App() {
+  async function getData() {  
+      const response= await fetch("https://jsonplaceholder.typicode.com/todos/1")
+      console.log(response);
+      
+    
+  }
   return (
     <>
+    <div className="">
+      <button 
+      className='bg-amber-50/50 p-3 rounded-2xl'
+      onClick={getData}
+      >Get Data
+      </button>
+    </div>
     </>
   )
 }
