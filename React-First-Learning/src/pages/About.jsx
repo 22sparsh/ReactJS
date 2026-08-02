b051a13b52de0841726f6ad6f832e38b7aa7473d
Sparsh Prachandia Jain <spjainprachandia@gmail.com>
2026-08-02 14:39:16 +0530

    useNavigate

--- React-First-Learning/src/pages/About.jsx
+++ React-First-Learning/src/pages/About.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const About = () => {
+  let navigate = useNavigate();
   return (
-    <div className='text-4xl font-bold'>About</div>
-  )
-}
+    <>
+      <button
+        onClick={()=>{
+          navigate('/')
+        }}
+        className="bg-indigo-600 text-2xl p-3 rounded-xl cursor-pointer active:scale-95"
+      >
+        Return to Home
+      </button>
+      <button
+        onClick={()=>{
+          navigate(-1)
+        }}
+        className="bg-indigo-600 ml-2 text-2xl p-3 rounded-xl cursor-pointer active:scale-95"
+      >
+        Back
+      </button>
+      <div className="text-4xl font-bold">About</div>
+    </>
+  );
+};
 
-export default About
+export default About;
